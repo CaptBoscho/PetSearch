24f5c6f9755a6293c0aedaf76eaa35984d50f4ce
Brandon <bvance800@gmail.com>
2016-09-27 19:10:44 +0000

    Added search api

--- search.js
+++ search.js
@@ -32,5 +32,108 @@ function searchA() {
     $('#youtubeVideo').attr('src',videoURL);;
     console.log('check');
  });
+
+	
+	var key = "223447a770589a3ea900c78154e2537e";
+
+	var petURL = "http://api.petfinder.com/";
+	var method2 = "pet.getRandom";
+	var argument = "animal=" + $('#query').val().toLowerCase();
+	var argument2 = "output=basic";
+
+	var fullURL = petURL + method2 + "?key=" + key + "&" + argument2 + "&"+argument+ "&format=json";
+	console.log(fullURL);	
+
+
+	$.ajax({
+
+		url:fullURL,
+		dataType:"jsonp",
+		success:function(data){
+			console.log(data);	
+			//var obj = JSON.parse(data);
+			//console.log(obj);	
+			var photos = $('#photos');
+			var list;	
+			var greeting;		
+			var name;
+			var breed = "";
+
+			if(data.petfinder.pet.name.hasOwnProperty("$t"))
+			{
+				name = data.petfinder.pet.name.$t;
+			}
+
+			if(data.petfinder.pet.breeds.breed.hasOwnProperty("$t"))
+			{
+				breed = data.petfinder.pet.breeds.breed.$t;
+			}
+			else
+			{
+				$.each(data.petfinder.pet.breeds.breed, function(i, item){
+					if(item.$t !== null){breed += item.$t + " ";}
+					console.log(item.$t);
+				});
+			
+			}
+			console.log(breed);
+			console.log(name);
+			greeting += "Meet ";
+			
+			if(name !== null)
+			{
+				greeting += name + " ";
+			}			
+			
+			if(breed !== "")
+			{
+
+				greeting += "the " + breed;
+			}
+
+			var count = 0;
+			var media;
+			greeting = greeting.replace("undefined","");
+			list = greeting + "<br>";
+					
+	
+			if(data.petfinder.pet.media.hasOwnProperty("photos") &&
+				data.petfinder.pet.media.photos.hasOwnProperty("photo"))
+			{
+				media = data.petfinder.pet.media.photos.photo;
+			}
+			else
+                        {
+
+                                //no photos available
+                               list += "<p>No Photos Available</p>";
+                        }
+
+			
+			if(media !== null && media !== undefined)
+			{
+				list += "<ul>";
+				$.each(media,function(i, item){
+				//	console.log(item.$t);
+					
+					count = count % 5;
+					
+				//	console.log(count);
+
+					if(count === 2){
+						
+						list += "<li><img src=" + item.$t +">";	
+		
+					}
+
+					count++;		
+				});
+				list += "</ul>";
+			}
+			
+			photos.html(list);
+		}	
+	});
+
 }
 });
